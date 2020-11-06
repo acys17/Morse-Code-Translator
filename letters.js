50a568ca1a59dec4be695be397753d7750b87613
Alice Scott <alice_cy_scott@hotmail.com>
2020-11-06 11:31:44 +0000

    changes made

---
+++ letters.js
@@ -0,0 +1,152 @@
+const morseLetters = [
+    {
+    name: "a",
+    code: ".- "
+    },
+    {
+    name: "b", 
+    code: "-... "
+    },
+    {
+    name: "c", 
+    code: "-.-. "
+    },
+    {
+    name: "d", 
+    code: "-.. "
+    },
+    {
+    name: "e", 
+    code: ". "
+    },
+    {
+    name: "f", 
+    code: "..-. "
+    },
+    {
+    name: "g", 
+    code: "--. "
+    },
+    {
+    name: "h", 
+    code: ".... "
+    },
+    {
+    name: "i", 
+    code: ".. "
+    },
+    {
+    name: "j", 
+    code: ".--- "
+    },
+    {
+    name: "k", 
+    code: "-.- "
+    },
+    {
+    name: "m", 
+    code: "-- "
+    },
+    {
+    name: "l", 
+    code: ".-.. "
+    },
+    {
+    name: "n", 
+    code: "-. "
+    },
+    {
+    name: "o", 
+    code: "--- "
+    },
+    {
+    name: "p", 
+    code: ".--. "
+    },
+    {
+    name: "q", 
+    code: "--.- "
+    },
+    {
+    name: "r", 
+    code: ".-. "
+    },
+    {
+    name: "s", 
+    code: "... "
+    },
+    {
+    name: "t", 
+    code: "- "
+    },
+    {
+    name: "u", 
+    code: "..- "
+    },
+    {
+    name: "v", 
+    code: "...- "
+    },
+    {
+    name: "w", 
+    code: ".-- "
+    },
+    {
+    name: "x", 
+    code: "-..- "
+    },
+    {
+    name: "y", 
+    code: "-.-- "
+    },
+    {
+    name: "z", 
+    code: "--.. "
+    },
+    {
+    name: " ", 
+    code: "  "
+    },
+    {
+    name: "1", 
+    code: ".---- "
+    },
+    {
+    name: "2", 
+    code: "..--- "
+    },
+    {
+    name: "3", 
+    code: "...-- "
+    },
+    {
+    name: "4", 
+    code: "....- "
+    },
+    {
+    name: "5", 
+    code: "..... "
+    },
+    {
+    name: "6", 
+    code: "-.... "
+    },
+    {
+    name: "7", 
+    code: "--... "
+    }, 
+    {
+    name: "8", 
+    code: "---.. "
+    },
+    {
+    name: "9", 
+    code: "----. "
+    },
+    {
+    name: "0", 
+    code: "----- "
+    },  
+];
+
+export default morseLetters;
---
+++ main.js
@@ -0,0 +1,28 @@
+import morseLetters from "./letters.js";
+
+const input = document.querySelector("#toBeTranslated");
+const form = document.querySelector("form");
+const output = document.querySelector("#output")
+
+const translate = () => {
+    // split input string into array of letters
+    const lowerCase = input.value.toLowerCase();
+    const inputArray = (lowerCase).split("");
+
+    // for each letter in array, find letter object and return morse code
+    const engToMorseArray = inputArray.map((letter) => {
+        const findLetterObject = morseLetters.find(element => element.name === letter);
+        return findLetterObject.code
+    })
+
+    // join morse letters back into string
+    const engToMorseStr = engToMorseArray.join("")
+    console.log(engToMorseStr);
+    // return engToMorseStr;
+    output.innerHTML = engToMorseStr;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    translate(); 
+});
